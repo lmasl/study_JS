0a8bc078b39e125a7c1aad700ae56db82f8ce25e
Liza Maslovskaya <maselnp@gmail.com>
2020-06-05 23:48:00 +0300

    lesson-1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+let income = 200;
+let addEpenses = 1000;
+let deposit = 100;
+let mission = 'abcde';
+let period = 30 ;
+let money = addEpenses - income - deposit;
+
+console.log ('Ваш остаток:' + money);
+
+alert('Hello world!')
