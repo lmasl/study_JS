98fce9251a35a0cabaeaac3f7d5db87cf590bd57
Liza Maslovskaya <maselnp@gmail.com>
2020-06-07 00:11:05 +0300

    lesson2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
+let money = 1000;
+let income = 'фриланс';
+let addExpenses = 'Аренда, такси, еда';
+let deposit = new Boolean(true);
+let mission = 3000;
+let period = 3 ;
 
-let income = 200;
-let addEpenses = 1000;
-let deposit = 100;
-let mission = 'abcde';
-let period = 30 ;
-let money = addEpenses - income - deposit;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-console.log ('Ваш остаток:' + money);
+console.log(addExpenses.length);
 
-alert('Hello world!')
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель заработать '+ mission + ' рублей');
+
+let splitString = addExpenses.split(",");
+console.log(splitString);
+
+let budgetDay = money/30;
+console.log('Ваш дневной бюджет составил:'+budgetDay);
